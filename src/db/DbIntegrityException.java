00826757e36c4acb0a33be5c3fbdb3139fd156ee
claudioRoliveira <c.rosa.oliveira@gmail.com>
2021-02-09 11:24:22 -0300

    db package

---
+++ src/db/DbIntegrityException.java
@@ -0,0 +1,15 @@
+package db;
+
+public class DbIntegrityException extends RuntimeException{
+
+	public DbIntegrityException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
